afb74df8e46f4761abd5a801e68dca4be60414d3
Amanda-Levy <amanda_levy@brown.edu>
2021-10-13 17:08:04 -0400

    fixed results

--- src/main/java/edu/brown/cs/student/main/Main.java
+++ src/main/java/edu/brown/cs/student/main/Main.java
@@ -170,7 +170,7 @@ public final class Main {
             //TODO: use the global autocorrect instance to get the suggestions
             Set<String> suggestions = ac.suggest(acEntry);
             //TODO: create an immutable map using the suggestions
-            Map vars = ImmutableMap.of(acEntry, suggestions);
+            Map vars = ImmutableMap.of("results", suggestions);
             //TODO: return a Json of the suggestions (HINT: use the GSON.Json())
             return GSON.toJson(vars);
         }
